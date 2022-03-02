a1de35ca6e17c308a99555111edb020a0ba7d58b
Bennet <totals@gmx.net>
2022-03-02 17:16:38 +0100

    removed listing and update from models

--- jobs/api/viewsets.py
+++ jobs/api/viewsets.py
@@ -1,19 +1,33 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from jobs.models import Company, JobPosting, Applicant
 from .serializer import CompanySerializer, JobPostingSerializer, ApplicantSerializer
 
 
-class CompanyViewSet(viewsets.ModelViewSet):
+# class CompanyViewSet(viewsets.ModelViewSet):
+#    queryset = Company.objects.all()
+#    serializer_class = CompanySerializer
+
+
+class CompanyViewSet(mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
 
-class JobPostingViewSet(viewsets.ModelViewSet):
+class JobPostingViewSet(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
     queryset = JobPosting.objects.all()
     serializer_class = JobPostingSerializer
 
 
-class ApplicantViewSet(viewsets.ModelViewSet):
+class ApplicantViewSet(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
     queryset = Applicant.objects.all()
     serializer_class = ApplicantSerializer
 
